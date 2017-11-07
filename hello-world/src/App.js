ab9e26d9bb9e9bed8906e42850ecd98fc7484096
Yamile Tabares <yamile08.lytm@gmail.com>
2017-11-07 16:41:38 -0500

    Animaciones con React Transicion Group

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import logoPlatzi from './platzi.png';
 import './App.css';
 import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 class App extends Component {
+  state = {
+    logo: logo,
+  }
+  onClick() {
+    this.setState({
+      logo: logoPlatzi,
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Amimaciones</h1>
-        </header>
+        <div className="App-header">
+          <CSSTransitionGroup
+            transitionName="fade"
+            transitionEnterTimeout={1000}
+            transitionLeaveTimeout={1000}
+          >
+            <img
+              key={this.state.logo}
+              src={this.state.logo}
+              className="App-logo"
+              alt="logo"
+            />
+          </CSSTransitionGroup>
+          <h2>Hola mundo!</h2>
+          <button onClick={this.onClick.bind(this)}>click me!</button>
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
